9e5ef0c5226fa66780f7d76d7396b1fe95c7d48c
sanjeev24433 <sanjeev24433@gmail.com>
2025-02-06 21:19:41 -0800

    implemented loads and stores

--- riscv32i.c
+++ riscv32i.c
@@ -6,15 +6,69 @@
 
 FILE *memFile;
 
-uint8_t stack[STACK_SIZE];
+int8_t memory[STACK_SIZE];
 uint16_t address;
-uint32_t instruction, pc = 0, gpr[32], instructionCount = 0, opcode;
+int instruction, pc = 0, gpr[32], instructionCount = 0, opcode;
 
-void store(int instruction) {
-    uint32_t function = (instruction >> 12) & 0x00000007;
-    uint32_t rs1 = (instruction >> 15) & 0x0000001F;
-    uint32_t rs2 = (instruction >> 20) & 0x0000001F;
-    int immediate = (instruction >> 7) & 0x0000001F;
+void store() {
+    int function = (instruction >> 12) & 0x7;
+    int rs1 = (instruction >> 15) & 0x1F;
+    int rs2 = (instruction >> 20) & 0x1F;
+    int upper = (instruction >> 31) ? 0xFFFFF000 : 0;
+    int middle = (instruction >> 20) & 0xFE0;
+    int lower = (instruction >> 7) & 0x1F;
+    int immediate = upper | middle | lower;
+    int byteAddress = immediate + gpr[rs1];
+
+    switch (function)
+    {
+    case 0:
+        memory[byteAddress] = gpr[rs2];
+        break;
+    case 1:
+        memory[byteAddress] = gpr[rs2];
+        memory[byteAddress + 1] = gpr[rs2] >> 8;
+        break;
+    case 2:
+        memory[byteAddress] = gpr[rs2];
+        memory[byteAddress + 1] = gpr[rs2] >> 8;
+        memory[byteAddress + 2] = gpr[rs2] >> 16;
+        memory[byteAddress + 3] = gpr[rs2] >> 24;
+        break;
+    default:
+        break;
+    }
+}
+
+void load() {
+    int function = (instruction >> 12) & 0x7;
+    int rs = (instruction >> 15) & 0x1F;
+    int rd = (instruction >> 7) & 0x1F;
+    int upper = (instruction >> 31) ? 0xFFFFF000 : 0;
+    int lower = (instruction >> 20) & 0xFFF;
+    int immediate = upper | lower;
+    int byteAddress = immediate + gpr[rs];
+
+    switch (function)
+    {
+    case 0:
+        gpr[rd] = memory[byteAddress];
+        break;
+    case 1:
+        gpr[rd] = (memory[byteAddress] | (memory[byteAddress + 1] << 8));
+        break;
+    case 2:
+        gpr[rd] = (memory[byteAddress] | (memory[byteAddress + 1] << 8) | (memory[byteAddress + 2] << 16) | (memory[byteAddress + 3] << 24));
+        break;
+    case 4:
+        gpr[rd] = memory[byteAddress] & 0xFF;
+        break;
+    case 5:
+        gpr[rd] = (memory[byteAddress] | (memory[byteAddress + 1] << 8)) & 0xFFFF;
+        break;
+    default:
+        break;
+    }
 }
 
 int main(int argc, char *argv[2]) {
@@ -35,28 +89,29 @@ int main(int argc, char *argv[2]) {
             fprintf(stderr, "Error: Address out of bounds (0x%hx)\n", address);
             continue;
         }
-        stack[address] = instruction & 0x000000FF;
-        stack[address + 1] = (instruction >> 8) & 0x000000FF;
-        stack[address + 2] = (instruction >> 16) & 0x000000FF;
-        stack[address + 3] = (instruction >> 24) & 0x000000FF;
-        instructionCount++;
+        memory[address] = instruction;
+        memory[address + 1] = instruction >> 8;
+        memory[address + 2] = instruction >> 16;
+        memory[address + 3] = instruction >> 24;
+        instructionCount += 4;
     }
 
-    // printf("Contents of Stack:\n");
-    // for (int i = 0; i <= instructionCount; i += 4) {
-    //     printf("%08x\n", (stack[i] | (stack[i + 1] << 8) | (stack[i + 2] << 16) | (stack[i + 3] << 24)));
-    // }
-
-    while ((pc / 4) < instructionCount) {
-        instruction = (stack[pc] | (stack[pc + 1] << 8) | (stack[pc + 2] << 16) | (stack[pc + 3] << 24));
-        opcode = instruction & 0x0000007F;
+    while (pc < instructionCount) {
+        instruction = (memory[pc] | (memory[pc + 1] << 8) | (memory[pc + 2] << 16) | (memory[pc + 3] << 24));
+        opcode = instruction & 0x7F;
         switch (opcode)
         {
         case 0x23:
-            store(instruction);
+            store();
+            pc += 4;
+            break;
+        case 0x3:
+            load();
+            pc += 4;
             break;
         default:
             pc += 4;
+            break;
         }
     }
 
