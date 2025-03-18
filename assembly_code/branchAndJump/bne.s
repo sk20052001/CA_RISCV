addf428c028fbad9e9b786110627f7028e9bc2a5
sk20052001 <sanjeev@pdx.edu>
2025-03-18 01:43:51 -0700

    added some branch testcodes and verified

---
+++ assembly_code/branchAndJump/bne.s
@@ -0,0 +1,15 @@
+  .section .text
+	.align 2
+	.globl	_start	
+
+_start:
+	addi x18, x18, 12
+	addi x19, x19, 11
+	bne x18, x19, taken
+	addi x10, x10, 20
+.word 0
+
+taken:
+	addi x20, x20, 1
+	addi x21, x21, 2
+.word 0
