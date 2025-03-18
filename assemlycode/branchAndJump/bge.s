50df1245e6b12260984ef9b35db51608ea468bf8
sk20052001 <sanjeev@pdx.edu>
2025-03-18 01:22:56 -0700

    jal and some branch verified and added

--- assemlycode/branchAndJump/bge.s
+++ assemlycode/branchAndJump/bge.s
@@ -5,8 +5,17 @@
 _start:
 	addi x18, x18, 12
 	addi x19, x19, 11
-	bge x18, x19, 8
+	bge x18, x19, taken
 	addi x10, x10, 20
 	addi x20, x20, 1
 	addi x21, x21, 2
+.word 0
+
+taken2:
+	addi x19, x19, 12
+.word 0
+
+taken:
+	addi x18, x18, -1
+	bge x18, x19, taken2
 .word 0
